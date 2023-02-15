575f2b0b91014ba3607a80b09bf7337e501ef9a2
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-15 19:27:13 +0000

    assignment complete

--- float_find_hypotenuse.rb
+++ float_find_hypotenuse.rb
@@ -1,8 +1,14 @@
 # A right triangle's two shorter sides have length 2.8 and 4.5. How long is its longest side?
 # Check the pythagorous.png file for the Pythagorean Theorem
-# 
+#
 # Your output should be a number; for example,
-# 
+#
 # 5.3
 
+a = 2.8 ** 2
 
+b = 4.5 ** 2
+
+c = a + b
+
+p c ** 0.5
